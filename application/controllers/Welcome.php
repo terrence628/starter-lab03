6e343dafe37115f8748d6a1d0fccd290ba2d3ade
terrence628 <terrence_lam@hotmail.com>
2015-09-24 15:49:25 -0700

    fix lock link

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -31,6 +31,23 @@ class Welcome extends Application {
         $this->render();
     }
 
+        function shucks() {
+        $this->data['pagebody'] = 'justone';    // this is the view we want shown
+        // build the list of authors, to pass on to our view
+        $source = $this->quotes->get(2);
+        
+        $this->data['mug'] = $source['mug'];
+        $this->data['who'] = $source['who'];
+        $this->data['what'] = $source['what'];
+        /*$authors = array();
+        foreach ($source as $record) {
+            $authors[] = array('who' => $record['who'], 'mug' => $record['mug'], 'href' => $record['where']);
+        }
+        $this->data['authors'] = $authors;*/
+
+        $this->render();
+    }
+
 }
 
 /* End of file Welcome.php */
